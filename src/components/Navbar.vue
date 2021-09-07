70d1e25ae65f6ef9e510f50404757292582debb1
dulecux <dusan.culibrk@live.com>
2021-09-07 16:37:58 +0200

    Auth is finished

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,26 @@
 <template>
-  <nav>
+  <nav v-if="user">
     <div>
-      <p>Hey there...</p>
-      <p class="email">Currently logged in as...</p>
+      <p>Hey there {{ user.displayName }}</p>
+      <p class="email">Currently logged in as {{ user.email }}</p>
     </div>
-    <button>Logout</button>
+    <button @click="handleClick">Logout</button>
   </nav>
 </template>
 
 <script>
+import useLogout from '../composables/useLogout'
+import getUser from '../composables/getUser'
 export default {
+  setup() {
+    const { logout, error } = useLogout()
+    const { user } = getUser()
+    
+    const handleClick = async () => {
+      await logout()
+    }
+    return { handleClick, user }
+  }
 }
 </script>
 
