50ab29c7d20dfdab7344c875082085516765f54e
dulecux <dusan.culibrk@live.com>
2021-09-07 18:01:06 +0200

    Chat room finished

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="container">
     <Navbar />
+    <ChatWindow />
+    <NewChatForm />
   </div>
 </template>
 
 <script>
+import NewChatForm from '../components/NewChatForm.vue'
 import Navbar from '../components/Navbar.vue'
+import ChatWindow from '../components/ChatWindow.vue'
 import getUser from '../composables/getUser'
 import { watch } from 'vue'
 import { useRouter } from 'vue-router'
 
 export default {
-  components: { Navbar },
+  components: { Navbar, NewChatForm, ChatWindow },
    setup() {
     const router = useRouter()
     const { user } = getUser()
